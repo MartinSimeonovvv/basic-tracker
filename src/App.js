78e474004f9a35db05adc0ca82c750d486a92b33
MartinSimeonovvv <marti_191997@abv.bg>
2022-01-11 11:25:03 +0200

    Creating Task module and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "D-r Appointment",
+      day: "Feb 5th at 2:30om",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Football Match",
+      day: "Feb 7th at 9:00om",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 10th at 8:30om",
+      reminder: false,
+    },
+  ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
